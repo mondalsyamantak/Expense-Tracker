a2447f7c1da56e898ddd518c0a7e8409670478d7
Syamantak Mondal <mondalsyamantak234@gmail.com>
2025-09-19 19:10:47 +0530

    configuring the transactions page

--- frontend/src/pages/site/routes/Transactions.jsx
+++ frontend/src/pages/site/routes/Transactions.jsx
@@ -1,8 +1,36 @@
 import React from 'react'
+import { DataTable } from '../transactionTable/data-table'
+import {columns} from '../transactionTable/columns'
+
+export const payments= [
+  {
+    id: "728ed52f",
+    amount: 100,
+    status: "pending",
+    email: "m@example.com",
+  },
+  {
+    id: "489e1d42",
+    amount: 125,
+    status: "processing",
+    email: "example@gmail.com",
+  },
+  // ...
+]
 
 function Transactions() {
   return (
-    <div>Transactions</div>
+    <main className="
+    flex-1 
+    p-6 space-y-6
+    ">
+      <div className="
+      w-full 
+      flex lg:flex-row flex-col flex-nowrap 
+      gap-6">
+        <DataTable columns={columns} data={payments}/>
+      </div>
+    </main>
   )
 }
 
